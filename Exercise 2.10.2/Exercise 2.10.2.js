81ba9adb67ea6adc367d4bc18fff8117c4e664bc
MikeTrick <mikhail.lapuzin@gmail.com>
2022-06-21 23:05:59 +0500

    Exercise 2.10.2

---
+++ Exercise 2.10.2/Exercise 2.10.2.js
@@ -0,0 +1,5 @@
+const officialName = prompt('Какое «официальное» название JavaScript?', 'Введите название')
+
+if (officialName === 'ECMAScript') {
+    alert('Верно!')
+} else alert('Не знаете? ECMAScript!')
