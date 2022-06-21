552220fbdef0ddc5178f5d80d276d38a0730ff34
MikeTrick <mikhail.lapuzin@gmail.com>
2022-06-21 23:28:23 +0500

    Exercise 2.10.3

---
+++ Exercise 2.10.3/Exercise 2.10.3.js
@@ -0,0 +1,9 @@
+let number = (+prompt('Введите любое число', '0'));
+
+if (number > 0) {
+    window.alert(1)
+} else if (number < 0) {
+    window.alert(-1)
+} else if (number === 0) {
+    window.alert(0)
+}
