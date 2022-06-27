9c763f8513701ff255fedc070234a0d6140f680a
MikeTrick <mikhail.lapuzin@gmail.com>
2022-06-27 17:44:26 +0500

    Exercise 5.2.4

---
+++ Exercise 5.2.4/Exercise 5.2.4.js
@@ -0,0 +1,8 @@
+function randomInteger(min, max) {
+    return Math.round((min - 0.5) + Math.random() * (max + 0.5));
+}
+
+alert(randomInteger(1, 5));
+alert(randomInteger(1, 5));
+alert(randomInteger(1, 5));
+alert(randomInteger(1, 5));
