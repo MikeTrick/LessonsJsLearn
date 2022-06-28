5a456f1c57afaa31493ab1e8c399cd46d540ba57
MikeTrick <mikhail.lapuzin@gmail.com>
2022-06-29 02:06:30 +0500

    Exercise 6.8.1

---
+++ Exercise 6.8.1/Exercise 6.8.1.js
@@ -0,0 +1,31 @@
+// setInterval
+
+const printNumbers = (from, to) => {
+    let current = from;
+
+    let timerId = setInterval(function () {
+        alert(current);
+        if (current === to) {
+            clearInterval(timerId);
+        }
+        current++;
+    }, 1000);
+}
+
+printNumbers(5, 10);
+
+// Рекурсивный setTimeout
+
+const printNumbers = (from, to) => {
+    let current = from;
+
+    setTimeout(function go() {
+        alert(current);
+        if (current < to) {
+            setTimeout(go, 1000);
+        }
+        current++;
+    }, 1000);
+}
+
+printNumbers(5, 10);
