a08ce4f47965703e50a466ece0fd37f1c1c0f19b
MikeTrick <mikhail.lapuzin@gmail.com>
2022-06-29 02:01:09 +0500

    Exercise 6.1.5

---
+++ Exercise 6.1.5/Exercise 6.1.5.js
@@ -0,0 +1,37 @@
+let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+};
+
+/*const printReverseList = (list) => {
+
+    if (list.next) {
+        printReverseList(list.next);
+    }
+    alert(list.value);
+}*/
+
+const printReverseList = (list) => {
+    let arr = [];
+    let tmp = list;
+
+    while (tmp) {
+        arr.push(tmp.value);
+        tmp = tmp.next;
+    }
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        alert(arr[i]);
+    }
+}
+
+printReverseList(list);
