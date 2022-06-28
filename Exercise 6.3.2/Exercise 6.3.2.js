02b95c01bff4914c155b10c6d8629723d1a2af2b
MikeTrick <mikhail.lapuzin@gmail.com>
2022-06-29 02:02:23 +0500

    Exercise 6.3.2

---
+++ Exercise 6.3.2/Exercise 6.3.2.js
@@ -0,0 +1,21 @@
+// Фильтр inBetween
+
+const inBetween = (a, b) => {
+    return function (x) {
+        return x >= a && x <= b;
+    };
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+alert(arr.filter(inBetween(3, 6)));
+
+//Фильтр inArray
+
+const inArray = (arr) => {
+    return function (x) {
+        return arr.includes(x);
+    };
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+alert(arr.filter(inArray([1, 2, 10])));
